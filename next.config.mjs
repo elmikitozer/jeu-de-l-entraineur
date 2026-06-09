6feaa6ff527076b2a3c73f955bc02cf11d677dd7
elmikitozer <mikayay@icloud.com>
2026-06-10 01:22:35 +0200

    feat: initial deploy Jeu de l'Entraîneur CdM 2026

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['flagcdn.com', 'media.api-sports.io'],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
